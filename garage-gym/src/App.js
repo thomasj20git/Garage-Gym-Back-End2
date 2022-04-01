92320c3c1454bb0a8821099292774b5835429622
Thomas Johnson <thomasj.johnson20@gmail.com>
2022-03-31 19:02:43 -0500

    all routes working

---
+++ garage-gym/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import GymPostContainer from './gymPostContainer/gymPostContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Garage Gym App!!!</h1>
+      <GymPostContainer></GymPostContainer>
+    </div>
+  );
+}
+
+export default App;
